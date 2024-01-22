885fbdbceb804e3c5c6123886fbd7d59992ac3cb
Behrooz Roozbahani <behroozroozbahani7@gmail.com>
2024-01-22 18:42:42 +0330

    Commit All Changes

--- Library Management System/EndPoint/obj/Debug/net6.0/EndPoint.AssemblyInfo.cs
+++ Library Management System/EndPoint/obj/Debug/net6.0/EndPoint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EndPoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+774c0c700925336b9a2b80def812f753774b5b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("EndPoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EndPoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
